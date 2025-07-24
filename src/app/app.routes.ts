83dd6dd869f2a561e84e271d19cba3cb7d80833d
Gemy33 <mohamedgamalabdelhaliym@gmail.com>
2025-07-24 22:33:15 +0200

    end from routing and add some ui features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/components/home/home';
+import { AllProducts } from './features/products/product/all-products/all-products';
+import { ContactUs } from './features/contact-us/contact-us';
+import { Cart } from './features/cart/cart/cart';
+import { NotFound } from './features/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'home',component:Home},
+    {path:'product',component:AllProducts},
+    {path:'contact',component:ContactUs},
+    {path:'cart',component:Cart},
+    {path:'**',component:NotFound}
+    
+];
